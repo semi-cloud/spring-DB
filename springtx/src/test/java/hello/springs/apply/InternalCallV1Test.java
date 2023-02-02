36fa7019cf667cba684b75cc7ff3308509d6859b
semi-cloud <sksmsrkdtpal@naver.com>
2023-02-02 11:09:52 +0900

    :white_check_mark: 트랜잭션 프록시 내부 호출 문제 및 해결 방법
    별도의 클래스로 분리를 통해 정상적으로 트랜잭션 수행되도록

---
+++ springtx/src/test/java/hello/springs/apply/InternalCallV1Test.java
@@ -0,0 +1,57 @@
+package hello.springs.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class = {}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internal();    // 내부에서 트랜잭션이 필요한 실체 객체 호출 => 적용되지 X
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("txActive = {}", txActive);
+        }
+    }
+}
---
+++ springtx/src/test/java/hello/springs/apply/InternalCallV2Test.java
@@ -0,0 +1,73 @@
+package hello.springs.apply;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV2Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class = {}", callService.getClass());
+    }
+
+    @Test
+    void externalCallV2() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+        @Bean
+        CallService callService() {
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
+        }
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class CallService {  // 프록시 생성 X
+        private final InternalService internalService;  // 프록시 객체가 주입
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internalService.internal();   // 내부 => 외부 호출로 변환하므로 트랜잭션 정상 적용
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("txActive = {}", txActive);
+        }
+    }
+
+    static class InternalService {   // 메서드를 클래스로 분리 : 프록시 생성
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("txActive = {}", txActive);
+        }
+    }
+}
