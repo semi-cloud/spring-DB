9f61fff2b228787e6d63ccba7359414d6c7059c4
semi-cloud <sksmsrkdtpal@naver.com>
2023-02-02 10:14:58 +0900

    :white_check_mark: 트랜잭션 적용 여부 확인 테스트

---
+++ springtx/src/test/java/hello/springs/apply/TxBasicTest.java
@@ -0,0 +1,61 @@
+package hello.springs.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest  // AOP 동작
+public class TxBasicTest {
+    @Autowired
+    BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class={}", basicService.getClass());
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        public BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    /**
+     * TransactionSynchronizationManager.isActualTransactionActive()
+     * => 현재 쓰레드에 트랜잭션이 적용되어 있는지 확인할 수 있는 기능(쓰레드 로컬)
+     */
+    static class BasicService {
+
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);   // True
+        }
+
+        public void nonTx() {
+            log.info("call nonTx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);   // False
+        }
+
+    }
+}
